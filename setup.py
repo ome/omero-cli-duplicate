898489eec67f33e9af3fbb940d0708b81bf30fe9
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-09-07 09:26:50 +0100

    Add classifier and remove file

--- setup.py
+++ setup.py
@@ -1,18 +1,63 @@
-from distribute_setup import use_setuptools
-use_setuptools()
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 University of Dundee.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Version: 1.0
+import os
 
 from setuptools import setup
 
 
+def read(fname):
+    """
+    Utility function to read the README file.
+    :rtype : String
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+version = '0.0.1'
+url = "https://github.com/ome/omero-cli-duplicate/"
+
 setup(
-    name='omero-cli-duplicate',
-    version='0.0.1',
-    author='',
-    author_email='',
+    version=version,
     packages=['omero.plugins'],
     package_dir={"": "src"},
-    url='',
-    license='See LICENSE.txt',
-    description='',
-    long_description=open('README.txt').read(),
+    name='omero-cli-duplicate',
+    description="Plugin for use in the OMERO CLI.",
+    long_description=read('README.rst'),
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Plugins',
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v2 '
+          'or later (GPLv2+)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],  # Get strings from
+          # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    author='The Open Microscopy Team',
+    author_email='ome-devel@lists.openmicroscopy.org.uk',
+    license='GPL-2.0+',
+    url='%s' % url,
+    download_url='%s/v%s.tar.gz' % (url, version),
+    keywords=['OMERO.CLI', 'plugin'],
 )
