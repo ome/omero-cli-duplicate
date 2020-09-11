b7c3b085fffd1fb3f9c98b22a0a51bd8e58344ab
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2020-09-11 09:17:07 +0100

    Remove obsolete text about missing pixel data.

--- src/omero/plugins/duplicate.py
+++ src/omero/plugins/duplicate.py
@@ -32,11 +32,6 @@ HELP = ("""Duplicate OMERO data.
 
 Duplicate entire graphs of data based on the ID of the top-node.
 
-Note that no object that corresponds to a file on disk will be duplicated.
-In some circumstances a duplicate object may reference an original object that
-does have associated binary data but a duplicated image should not be expected
-to include any pixel data.
-
 Examples:
 
     # Duplicate a dataset
