9cf2afb8fb5f2ad68b0eef9531668f7396efdccc
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2020-09-24 16:03:19 +0100

    Bump version: 0.4.0 → 0.4.1.dev0

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-version = '0.4.0'
+version = '0.4.1.dev0'
 url = "https://github.com/ome/omero-cli-duplicate/"
 
 setup(
